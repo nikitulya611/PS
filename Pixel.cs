c4b9fbeed794b90b14e7af34b0f2b6e3cd196ab0
nikitulya611 <nustyuzhanin19@gmail.com>
2020-10-31 03:44:14 +0300

    Add Class Pixel

---
+++ Pixel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MyPhotoshop
+{
+    public class Pixel
+    {
+        public Pixel()
+        {
+            R = 0;
+            G = 0;
+            B = 0;
+        }
+        public double R;
+        public double G;
+        public double B;   
+    }
+}
