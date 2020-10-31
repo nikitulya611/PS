63b09a874ebd6d094fde8a148aa3d249a7682c5d
nikitulya611 <nustyuzhanin19@gmail.com>
2020-10-31 04:42:33 +0300

    Fix Photo and Pixel

--- Data/Photo.cs
+++ Data/Photo.cs
@@ -4,9 +4,19 @@ namespace MyPhotoshop
 {
 	public class Photo
 	{
-		public int width;
-		public int height;
-		public Pixel[,] data;
+		public Photo(int _width, int _height)
+        {
+			width = _width;
+			height = _height;
+			data = new Pixel[width, height];
+			for (int x = 0; x < width; x++)
+				for (int y = 0; y < height; y++)
+					data[x, y] = new Pixel();
+        }
+
+		public readonly int width;
+		public readonly int height;
+		public readonly Pixel[,] data;
 	}
 }
 
--- Pixel.cs
+++ Pixel.cs
@@ -7,14 +7,52 @@ namespace MyPhotoshop
 {
     public class Pixel
     {
-        public Pixel()
+        private double r;
+        private double g;
+        private double b;
+
+        public static double Trim(double value)
         {
-            R = 0;
-            G = 0;
-            B = 0;
+            if (value < 0) return 0;
+            if (value > 1) return 1;
+            return value;
         }
-        public double R { get; set; }
-        public double G { get; set; }
-        public double B { get; set; }   
+
+        public double R {
+            get
+            {
+                return r;
+            } 
+            set
+            {
+                if (value < 0 || value > 1)
+                    throw new Exception(string.Format("Wrong channel R value {0} (the value must be between 0 and 1", value));
+                r = value;
+            }
+        }
+        public double G {
+            get
+            {
+                return g;
+            }
+            set
+            {
+                if (value < 0 || value > 1)
+                    throw new Exception(string.Format("Wrong channel G value {0} (the value must be between 0 and 1", value));
+                g = value;
+            }
+        }
+        public double B {
+            get
+            {
+                return b;
+            }
+            set
+            {
+                if (value < 0 || value > 1)
+                    throw new Exception(string.Format("Wrong channel B value {0} (the value must be between 0 and 1", value));
+                b = value;
+            }
+        }   
     }
 }
